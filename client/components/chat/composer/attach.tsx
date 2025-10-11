f129a38fc4696617705a9abc58896164eeaa4413
EhsanEDev <ehsan.ekhtiyar.pro@gmail.com>
2025-10-11 10:39:13 +0330

    fix: update button hover styles to ensure consistent behavior across components

--- client/components/chat/composer/attach.tsx
+++ client/components/chat/composer/attach.tsx
@@ -6,7 +6,7 @@ const AttachMenu: React.FC = () => {
     <Button
       variant="ghost"
       size="icon"
-      className="rounded-full cursor-pointer hover:bg-transparent focus-visible:ring-0 group"
+      className="rounded-full cursor-pointer hover:bg-transparent! focus-visible:ring-0 group"
       aria-label="Attach menu"
     >
       <PaperclipIcon className="size-6 text-muted-foreground group-hover:text-primary group-focus-visible:text-primary" />
--- client/components/chat/composer/emoji.tsx
+++ client/components/chat/composer/emoji.tsx
@@ -6,7 +6,7 @@ const EmojiPicker: React.FC = () => {
     <Button
       variant="ghost"
       size="icon"
-      className="rounded-full cursor-pointer hover:bg-transparent focus-visible:ring-0 group"
+      className="rounded-full cursor-pointer hover:bg-transparent! focus-visible:ring-0 group"
       aria-label="Pick an emoji"
     >
       <SmileIcon className="size-6 text-muted-foreground group-hover:text-primary group-focus-visible:text-primary" />
